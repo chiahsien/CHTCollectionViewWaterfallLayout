c3d6cc8f5b06f2ae25032abaff746e70a840b61e
Neil Kimmett <neilkimmett@gmail.com>
2013-10-21 16:48:32 +0100

    Add header support

---
+++ Demo/Demo/CHTCollectionViewWaterfallHeader.h
@@ -0,0 +1,13 @@
+//
+//  CHTCollectionViewWaterfallHeader.h
+//  Demo
+//
+//  Created by Neil Kimmett on 21/10/2013.
+//  Copyright (c) 2013 Nelson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CHTCollectionViewWaterfallHeader : UICollectionReusableView
+
+@end
