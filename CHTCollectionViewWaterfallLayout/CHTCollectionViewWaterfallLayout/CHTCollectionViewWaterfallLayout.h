fcea9e89faa984e1eb59c9f691c258f6f8004290
Jake Marsh <jake@deallocatedobjects.com>
2016-03-24 10:30:45 -0700

    Import main header.

--- CHTCollectionViewWaterfallLayout/CHTCollectionViewWaterfallLayout/CHTCollectionViewWaterfallLayout.h
+++ CHTCollectionViewWaterfallLayout/CHTCollectionViewWaterfallLayout/CHTCollectionViewWaterfallLayout.h
@@ -17,3 +17,4 @@ FOUNDATION_EXPORT const unsigned char CHTCollectionViewWaterfallLayoutVersionStr
 // In this header, you should import all the public headers of your framework using statements like #import <CHTCollectionViewWaterfallLayout/PublicHeader.h>
 
 
+#import <CHTCollectionViewWaterfallLayout/CHTCollectionViewWaterfallLayout.h>
